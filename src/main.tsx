6e44fe522f358ca70685c724371fa9351cf4160d
leon <de.nizard@hotmail.com>
2023-09-21 19:20:18 -0300

    Configured React Router in the project and set up the initial login route.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,32 @@
 import "../src/modules/login/styles/root.css";
 import "boxicons";
 
+import type { Router as RemixRouter } from "@remix-run/router";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from "react-router-dom";
 
-import App from "./App.tsx";
+import { loginRoutes } from "./modules/login/routes";
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <h1>Main</h1>,
+    errorElement: <h1>Pagina não encontrda</h1>,
+  },
+];
+
+const router: RemixRouter = createBrowserRouter([
+  ...loginRoutes,
+  ...mainRoutes,
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
